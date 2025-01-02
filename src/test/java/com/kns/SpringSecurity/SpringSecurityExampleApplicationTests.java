193bbf0051c4e7e6dce71a30c6aae446d40565b6
sudheer-kota <kotansudheer@gmail.com>
2025-01-02 23:24:25 +0530

    spring security by naveen reddy-initial project

---
+++ src/test/java/com/kns/SpringSecurity/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kns.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
