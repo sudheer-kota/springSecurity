95b2dd17bd22e30049b27d1f69e3c5fb77722951
sudheer-kota <kotansudheer@gmail.com>
2025-01-02 23:54:40 +0530

    session id output added to HelloController.java

--- src/main/java/com/kns/SpringSecurity/controller/HelloController.java
+++ src/main/java/com/kns/SpringSecurity/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.kns.SpringSecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String message() {
+   /* public String message() {
         return "Sreerama Rama Rama";
+    }**/
+    public String message(HttpServletRequest request) {
+        return "Sreerama Rama Rama " + request.getSession().getId();
     }
 }
+
